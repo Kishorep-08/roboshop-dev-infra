1f0b35ba92a9a080434896b4275e405ed8acd598
kishorep-08 <kishore.pkdp@gmail.com>
2025-10-29 20:07:12 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -36,6 +36,16 @@ resource "aws_security_group_rule" "redis_bastion" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  security_group_id = local.redis_sg_id  # mongodb SG
+  security_group_id = local.redis_sg_id  # redis SG
+  source_security_group_id = local.bastion_sg_id # Bastion sg
+}
+
+# RabbitMQ accepting connections from bastion
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.rabbitmq_sg_id  # rabbitmq SG
   source_security_group_id = local.bastion_sg_id # Bastion sg
 }
