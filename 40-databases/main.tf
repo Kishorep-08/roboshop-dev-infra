94d1b3b6c8dbca06f310cb43f497b36229c13353
kishorep-08 <kishore.pkdp@gmail.com>
2025-10-30 20:47:20 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mysql_sg_id]
   subnet_id = local.database_subnet_ids
-  iam_instance_profile = aws_iam_instance_profile.mysql.name
+  iam_instance_profile = "EC2SSM"
   tags = merge (
     local.common_tags,
     {
