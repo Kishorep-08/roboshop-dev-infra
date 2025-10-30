618284bc19a9105fda08dfcc4b041c9f498ee140
kishorep-08 <kishore.pkdp@gmail.com>
2025-10-30 17:23:59 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# dnf install ansible -y
+dnf install ansible -y
 # mkdir /var/log/roboshop/
 # touch ansible.log
 # ansible-pull -U https://github.com/Kishorep-08/ansible-roboshop-roles-tf.git main.yaml
