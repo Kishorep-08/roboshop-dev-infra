7a111d61e40ca92448e4edeca47e29083e622544
kishorep-08 <kishore.pkdp@gmail.com>
2025-10-31 21:17:47 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -69,3 +69,13 @@ resource "aws_security_group_rule" "catalogue_bastion" {
   security_group_id = local.catalogue_sg_id  # catalogue SG
   source_security_group_id = local.bastion_sg_id # Bastion sg
 }
+
+# MongoDB accepting connections from catalogue
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  security_group_id = local.mongodb_sg_id  # catalogue SG
+  source_security_group_id = local.catalogue_sg_id # Bastion sg
+}
