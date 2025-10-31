d89e331241f3e35a26e92d9ab6275e30b337db10
kishorep-08 <kishore.pkdp@gmail.com>
2025-10-31 18:41:35 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -7,7 +7,7 @@ locals {
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-    zone_id = data.aws_route53_zone.hosted_zone_id.value
+    # zone_id = data.aws_route53_zone.hosted_zone_id.value
 }
 
 
