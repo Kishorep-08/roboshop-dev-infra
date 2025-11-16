eda35d3ab4e725bc844fc2a5ea5e1d7fb42c21f1
kishorep-08 <kishore.pkdp@gmail.com>
2025-11-16 13:31:34 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -125,7 +125,7 @@ resource "aws_autoscaling_group" "catalogue" {
     id      = aws_launch_template.catalogue.id
     version = aws_launch_template.catalogue.latest_version
   }
-  vpc_zone_identifier       = local.private_subnet_ids
+  vpc_zone_identifier       = local.vpc_zone_identifier
   target_group_arns         = [aws_lb_target_group.catalogue]
 
   dynamic "tag" {
