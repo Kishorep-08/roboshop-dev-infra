88812e30d98a798bc6fe1774509681c9fd4aa58c
kishorep-08 <kishore.pkdp@gmail.com>
2025-11-16 13:25:58 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -57,3 +57,102 @@ resource "aws_ami_from_instance" "catalogue-ami" {
     }
   )
 }
+
+# Target group for catalogue
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${local.common_name}-catalogue-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60
+  health_check {
+    path                = "/health"
+    protocol            = "HTTP"
+    matcher             = "200-299"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  } 
+}
+
+# Launch template for catalogue autoscaling
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name}-catalogue"
+
+  image_id = "aws_ami_from_instance.catalogue-ami.id"
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = ["local.catalogue_sg_id"]
+
+  tag_specifications {
+    resource_type = "instance"
+    # Tags for EC2 instances launched from this template
+    tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name}-catalogue"
+    }
+  )
+  }
+ # Launch template tags
+ tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name}-catalogue"
+    }
+  )  
+}
+
+# Autoscaling group for catalogue
+resource "aws_placement_group" "test" {
+  name     = "test"
+  strategy = "cluster"
+}
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.common_name}-catalogue"
+  max_size                  = 5
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = aws_launch_template.catalogue.latest_version
+  }
+  vpc_zone_identifier       = local.private_subnet_ids
+  target_group_arns         = [aws_lb_target_group.catalogue]
+
+  dynamic "tag" {
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+    )
+    content {
+      key                 = "each.key"
+      value               = "each.value"
+      propagate_at_launch = true
+    }
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = "${aws_autoscaling_group.catalogue.name}"
+  name                   = "${local.common_name}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 75.0
+  }
+}
