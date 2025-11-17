3042a14104f6e6ae312219f1b4ddaf56476cbaef
kishorep-08 <kishore.pkdp@gmail.com>
2025-11-17 14:18:28 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -30,6 +30,6 @@ data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
 
-data "aws_ssm_parameter" "vpc_id" {
+data "aws_ssm_parameter" "backend_listener_arn" {
   name = "/${var.project_name}/${var.environment}/backend_listener_arn"
 }
